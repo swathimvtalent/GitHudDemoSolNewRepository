ad17d8aa7bfda56cd909ffe5a85c5c1536d87a71
Swathi Maddishetti <swathim.vtalent@gmail.com>
2024-02-22 14:46:35 +0530

    Added New Folder and create new class and create add method in side the class

---
+++ GitHudDemoProjectNew/Models/Sample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHudDemoProjectNew.Models
+{
+    public class Sample
+    {
+        public int Add(int x,int y)
+        {
+            return x + y;
+        }
+    }
+}
